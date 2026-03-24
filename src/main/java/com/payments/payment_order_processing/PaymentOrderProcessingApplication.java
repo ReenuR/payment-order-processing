81529955999d69c258efead4dd16de2e43f18c34
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-03-24 09:49:22 -0700

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/payments/payment_order_processing/PaymentOrderProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.payments.payment_order_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentOrderProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentOrderProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payments/payment_order_processing/PaymentOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.payment_order_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
