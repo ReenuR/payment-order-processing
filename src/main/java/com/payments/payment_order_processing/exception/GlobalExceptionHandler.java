fcd16ecb1da9b0a8beea3d5a2e192c58abca6e57
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-03-28 09:09:11 -0700

    feat: add GlobalExceptionHandler for consistent error responses

--- src/main/java/com/payments/payment_order_processing/exception/GlobalExceptionHandler.java
+++ src/main/java/com/payments/payment_order_processing/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.payments.payment_order_processing.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PaymentDeclinedException.class)
+    public ResponseEntity<String> handlePaymentDeclined(PaymentDeclinedException e){
+        return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(PaymentProcessingException.class)
+    public ResponseEntity<String> handlePaymentProcessingException(PaymentProcessingException e){
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong. Please try again later.");
+    }
 }
